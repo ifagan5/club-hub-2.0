f9b8fffdfcd110dff94c850befec787e75aedba8
hopingcandle <146850286+hopingcandle@users.noreply.github.com>
2025-10-23 15:14:53 -0400

    Updated Firebase Configuration

--- clubinfo.js
+++ clubinfo.js
@@ -5,15 +5,16 @@ import { initializeApp } from
 import { getFirestore, collection, collectionGroup, addDoc, getDocs,getDoc, doc, updateDoc, deleteDoc, setDoc, Timestamp, query, where, orderBy } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 import {updatePoints} from "./leaderboardScore.js";
 import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged , signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
-
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-apiKey: "AIzaSyAH3oWF9S-ePd0352Ca-TdE5cu6oinzlXo",
-authDomain: "softwareengineering-94854.firebaseapp.com",
-projectId: "softwareengineering-94854",
-storageBucket: "softwareengineering-94854.appspot.com",
-messagingSenderId: "565847408909",
-appId: "1:565847408909:web:9e116dae6ede6b965bb044"
+  apiKey: "AIzaSyDKBBs0TWerQno_u8yjNqV5qmvQImf6xA0",
+  authDomain: "club-hub-2.firebaseapp.com",
+  projectId: "club-hub-2",
+  storageBucket: "club-hub-2.firebasestorage.app",
+  messagingSenderId: "339870020143",
+  appId: "1:339870020143:web:cc698c287ed642e3798cda",
+  measurementId: "G-P97ML6ZP15"
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
