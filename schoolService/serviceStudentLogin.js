c86ce5cf125c3aa4a11d0d0d96273950873de766
jake <jmclaughlin28@students.stab.org>
2025-11-12 11:22:00 -0500

    minor changes

--- schoolService/serviceStudentLogin.js
+++ schoolService/serviceStudentLogin.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
 import { getFirestore, collection, collectionGroup, addDoc, getDocs,getDoc, doc, updateDoc, deleteDoc, setDoc, Timestamp, query, where, orderBy } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
-import {updatePoints} from "./leaderboardScore.js";
 import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged , signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
 
 //haha
@@ -18,21 +17,25 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-
-export const studentLogin = async function(email, pass) {
-    console.log("staritng login");
-        signInWithEmailAndPassword(auth, email, pass)
-            .then((userCredential) => {
-                // Signed in
-                const user = userCredential.user;
-                window.location.href = "serviceStudentPage.html";
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorCode + " - " + errorMessage)
-            });
-}
-
-//testing auto login
-studentLogin("example@example.com", "example").catch(e => console.error("Auto-login failed:", e));
+export async function sLogin() {
+    const uid = sessionStorage.getItem("student");
+    const enteredPassword = localStorage.getItem("password");
+    console.log(uid + " " + enteredPassword);
+    const docRef = doc(db, "students", uid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const studentData = docSnap.data();
+      if (enteredPassword === studentData.password) {
+        console.log("student login working");
+        location.replace("serviceStudentPage.html");
+      } 
+      else {
+        console.log("wrong username/password");
+        alert("Wrong Username or Password");
+      }
+    } 
+    else {
+      console.log("Student not found");
+      alert("Student not found");
+    }
+  }
