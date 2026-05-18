ebfada08bc5f3b8c87bbeb378b7a8d2ed4596514
jake <jmclaughlin28@students.stab.org>
2026-05-18 11:34:46 -0400

    unc detector 3.0

--- schoolService/serviceAuth.js
+++ schoolService/serviceAuth.js
@@ -83,7 +83,7 @@ export async function createUser(email, password, firstName, lastName, gradYr) {
     try {
         const isAdmin = false;
 
-        if (gradYr === "2025" || "25") {
+        if (parseInt(gradYr) <= 2025 || parseInt(gradYr) <= 25) {
             alert("ERROR: Unc Detected. Exiting...")
             return;
         }
